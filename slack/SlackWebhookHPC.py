988483ea3f5d9d45663505420732af1b874aeca3
tasotasoso <nayuta.suzuki@gmail.com>
2020-08-07 22:04:42 +0900

    add commit and test with mock of responses

--- slack/SlackWebhookHPC.py
+++ slack/SlackWebhookHPC.py
@@ -8,11 +8,24 @@ import requests
 
 
 class Client:
+    """Base Class for posts your message to your external app.
+    """
+
     def __init__(self, url_env: str) -> None:
         self.url_env = url_env
         self.common_failed_msg = "Failed to notificate."
 
     def post(self, text: str) -> str:
+        """Post message to external app.
+        This function posts your message to your external app.
+        URL for post is got by enviroment variable your had set.
+
+        Args:
+            text(str): your message for Slack Webhook app.
+
+        Return:
+             result_message(str): Result of post. 
+        """
         params = {"text": text}
 
         try:
@@ -23,7 +36,8 @@ class Client:
                 'catch KeyError:',
                 e)
             print("There are no URL, please set.")
-            return "ng_key_error"
+            result_message = "ng_key_error"
+            return result_message
 
         try:
             r = requests.post(url, data=json.dumps(params))
@@ -42,16 +56,22 @@ class Client:
             error_msg = "HTTP {} ERROR!".format(r.status_code)
             print(self.common_failed_msg + "{} error occur.".format(error_msg))
 
-        return r.text
+        result_message = r.text
+        return result_message
 
 
 class Slack(Client):
+    """Class for Slack webhook app to posts your message.
+    """
+
     def __init__(self, url_env: str) -> None:
         super().__init__(url_env)
         self.common_failed_msg = "Failed to notificate to your slack web hook app."
 
     @contextlib.contextmanager
     def post_with_time(self, text: str) -> Iterator[None]:
+        """Post your message to your Slack webhook app with time in "with" syntax.
+        """
         st = time.time()
         try:
             yield
--- tests/test_slack.py
+++ tests/test_slack.py
@@ -1,12 +1,20 @@
+import os
 import unittest
 
+import responses
 from slack import Slack
 
 
 class TestSlack(unittest.TestCase):
 
+    @responses.activate
     def test_post(self):
-        slack = Slack("slack_webhook_url")
+        slack_webhook_url = "https://hooks.slack.com/services/XXXXXXXXX/XXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXXX"
+        os.environ["url_env"] = slack_webhook_url
+        responses.add(responses.POST, slack_webhook_url,
+                      body="ok", status=200)
+
+        slack = Slack("url_env")
         response_text = slack.post("test")
         self.assertEqual("ok", response_text)
 
@@ -14,6 +22,7 @@ class TestSlack(unittest.TestCase):
         slack = Slack("no_url_env")
         response_text = slack.post("test")
         self.assertEqual("ng_key_error", response_text)
+
 
 if __name__ == '__main__':
     unittest.main()
